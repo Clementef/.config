4c4c78633d2c53397858da176148454f2231fdc9
clementef <mail@clementef.com>
2025-04-23 20:42:28 +0000

    add lsp, lint, autocomplete, mason
    basic setup for c, cpp, and lua

---
+++ nvim/lua/plugins/conform-nvim.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>u", function()
+			require("conform").format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+			print("autoformatting")
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
---
+++ nvim/lua/plugins/nvim-mason-lspconfig.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd", "lua_ls" } -- adjust as needed
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.clangd.setup({ })
+	  lspconfig.lua_ls.setup({ })
+    end
+  }
+}
