cc4048bb22e1915e407df333aa4af68d2d16121b
clementef <mail@clementef.com>
2025-05-01 00:57:15 +0000

    move conform to lspconfig

--- nvim/lua/plugins/nvim-mason-lspconfig.lua
+++ nvim/lua/plugins/nvim-mason-lspconfig.lua
@@ -10,24 +10,47 @@ return {
 		dependencies = { "williamboman/mason.nvim" },
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "clangd", "lua_ls" }, -- adjust as needed
+				ensure_installed = {
+					"lua_ls", --lua
+				}, -- adjust as needed
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"stevearc/conform.nvim",
+		},
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.clangd.setup({})
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
+			lspconfig.lua_ls.setup({})
+			lspconfig.html.setup({})
+			lspconfig.cssls.setup({})
+			-- lspconfig.ast_grep.setup({})
+			-- Autoformatting Setup
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					c = { "clang-format" },
+					html = { "prettierd" },
+					css = { "prettierd" },
 				},
+			})
+
+			-- autoformat on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				callback = function(args)
+					require("conform").format({
+						bufnr = args.buf,
+						lsp_fallback = true,
+						quiet = true,
+					})
+				end,
 			})
 		end,
 	},
