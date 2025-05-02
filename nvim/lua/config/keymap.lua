486bb6b72435412be54271ff571caf591b1b14b7
clementef <mail@clementef.com>
2025-05-02 01:24:41 +0000

    add toggleterm and binds
    binds use alt modifier, look into possible conflicts

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -1,22 +1,22 @@
 -- mapping functions
 function nmap(shortcut, command)
-  vim.api.nvim_set_keymap("n", shortcut, command, { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", shortcut, command, { noremap = true, silent = true })
 end
 
 function vmap(shortcut, command)
-  vim.api.nvim_set_keymap("v", shortcut, command, { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("v", shortcut, command, { noremap = true, silent = true })
 end
 
 function mapcmd(mode, shortcut, command, opts)
-    vim.api.nvim_set_keymap(mode, shortcut, "<CMD>" .. command .. "<CR>", opts)
+	vim.api.nvim_set_keymap(mode, shortcut, "<CMD>" .. command .. "<CR>", opts)
 end
 
 function nmapcmd(shortcut, command)
-    mapcmd("n", shortcut, command, { noremap = true, silent = true })
+	mapcmd("n", shortcut, command, { noremap = true, silent = true })
 end
 
 function vmapcmd(shortcut, command)
-    mapcmd("v", shortcut, command, { noremap = true, silent = true })
+	mapcmd("v", shortcut, command, { noremap = true, silent = true })
 end
 
 -- centered scrolling
@@ -25,51 +25,58 @@ nmap("<C-d>", "<C-d>zz")
 nmap("<C-u>", "<C-u>zz")
 
 -- -- toggle line wrap
-nmapcmd('<leader>w', 'set wrap!')
+nmapcmd("<leader>w", "set wrap!")
 
 -- -- copy to system clipboard
-nmap('<leader>y', '"+y')
-vmap('<leader>y', '"+y')
+nmap("<leader>y", '"+y')
+vmap("<leader>y", '"+y')
+
+--paste to system clipboard
+nmap("<leader>p", '"+p')
+vmap("<leader>p", '"+p')
 
 -- -- remove highlights
 -- nmapcmd(' ', 'nohl')
-nmapcmd('<ESC>', 'nohl')
+nmapcmd("<ESC>", "nohl")
 
 -- save
-nmapcmd('<C-s>', 'w')
+nmapcmd("<C-s>", "w")
+
+-- exit terminal input mode
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
 -- create splits
-nmap('<leader>v', '<C-w>v')
-nmap('<leader>V', '<C-w>s')
+nmap("<leader>v", "<C-w>v")
+nmap("<leader>V", "<C-w>s")
 
 -- Lazy
-nmapcmd('<leader>dl','Lazy')
+nmapcmd("<leader>dl", "Lazy")
 
 -- StartupTime
-nmapcmd('<leader>ds','StartupTime')
+nmapcmd("<leader>ds", "StartupTime")
 
 -- check health
-nmapcmd('<leader>dh', 'checkhealth')
+nmapcmd("<leader>dh", "checkhealth")
 
 -- Mason
 nmapcmd("<leader>dm", "Mason")
 
 -- Oil
-nmapcmd('<leader>o', 'Oil')
+nmapcmd("<leader>o", "Oil")
 
 -- zen
-nmapcmd('<leader>z', 'ZenMode')
+nmapcmd("<leader>z", "ZenMode")
 
 -- move lines up and down in visual mode
-vmap('J', ":m '>+1<CR>gv=gv")
-vmap('K', ":m '<-2<CR>gv=gv")
+vmap("J", ":m '>+1<CR>gv=gv")
+vmap("K", ":m '<-2<CR>gv=gv")
 
 -- indent and unindent in visual mode
 vmap("H", "<gv")
 vmap("L", ">gv")
 
 -- D deletes line contents
-nmap('D', '0d$')
+nmap("D", "0d$")
 
 -- telescope
 -- local telescope = require('telescope.builtin')
@@ -81,32 +88,12 @@ mapcmd("n", "<leader>l", "Telescope current_buffer_fuzzy_find theme=dropdown", {
 mapcmd("n", "<leader>r", "Telescope oldfiles theme=dropdown", {})
 mapcmd("n", "<leader>m", "Telescope man_pages theme=dropdown", {})
 
--- -- telescope file browser
--- mapcmd("n", "<leader>fj", "Telescope file_browser path=%:p:h select_buffer=true", {})
--- mapcmd("n", "<leader>e", "Telescope file_browser select_buffer=true", {})
-
--- -- set working directory
--- mapcmd('n', '<leader>p', 'cd %:p:h', {noremap = true})
--- mapcmd('n', '<leader>q', 'q', {noremap = true})
-
--- -- delete current buffer
--- nmapcmd('<leader>bq', 'bd')
-
--- split navigation
--- vim.keymap.set('n', '<A-h>', '<C-w>h', { noremap = true, silent = true })
--- vim.keymap.set('n', '<A-j>', '<C-w>j', { noremap = true, silent = true })
--- vim.keymap.set('n', '<A-k>', '<C-w>k', { noremap = true, silent = true })
--- vim.keymap.set('n', '<A-l>', '<C-w>l', { noremap = true, silent = true })
-
--- resize splits
--- nmap('<C-Left>', '<C-w><')
--- nmap('<C-Down>', '<C-w>+')
--- nmap('<C-Up>', '<C-w>-')
--- nmap('<C-Right>', '<C-w>>')
-
--- rebind command
--- nmap(';',':')
-
--- insert line in normal mode
--- nmap('<A-m>', 'o<ESC>')
--- nmap('<A-n>', 'O<ESC>')
+-- toggleterm
+vim.keymap.set("n", "<a-t>", "<CMD>" .. "ToggleTerm" .. "<CR>")
+vim.keymap.set("t", "<a-t>", "<CMD>" .. "ToggleTerm" .. "<CR>")
+vim.keymap.set("n", "<a-f>", "<CMD>" .. "ToggleTerm direction=float" .. "<CR>")
+vim.keymap.set("t", "<a-f>", "<CMD>" .. "ToggleTerm direction=float" .. "<CR>")
+vim.keymap.set("n", "<a-b>", "<CMD>" .. "ToggleTerm direction=horizontal" .. "<CR>")
+vim.keymap.set("t", "<a-b>", "<CMD>" .. "ToggleTerm direction=horizontal" .. "<CR>")
+vim.keymap.set("n", "<a-v>", "<CMD>" .. "ToggleTerm direction=vertical size=40" .. "<CR>")
+vim.keymap.set("t", "<a-v>", "<CMD>" .. "ToggleTerm direction=vertical size=40" .. "<CR>")
